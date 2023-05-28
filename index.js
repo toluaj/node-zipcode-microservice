a1b988f190717243ade6f51cb1921032f0986ce2
Tolu Ajia <toluajia@gmail.com>
2023-05-28 19:26:30 +0100

    chore: make routes accessible

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require("express");
+const routes = require("./api/routes");
 const PORT = process.env.PORT || 4000;
 
 const app = express();
+
+routes(app);
 
 app.listen(PORT, () => {
   console.log(`Server is listening on port: ${PORT}`);
